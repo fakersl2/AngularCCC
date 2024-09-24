6b127c1ba278fa99134d168b40c496703adee414
fakersl2 <projetotccfigma@outlook.com>
2024-09-24 07:39:04 -0300

    Criando as rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,24 @@
 /* ARQUIVO RESPONSAVEL POR MANIPULAR AS ROTAS DO SITE */
 
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProdutosComponent } from './produtos/produtos.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent,
+    },
+    {
+
+        path: 'home',
+        component: HomeComponent,
+    },
+    {
+
+        path: 'produtos',
+        component: ProdutosComponent,
+    }
+];
